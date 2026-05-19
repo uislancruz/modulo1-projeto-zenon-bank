5910d1c7a7ef87a1b2da57c00b0985fb525e7a61
Uislan Cruz <uislansk8@hotmail.com>
2026-05-19 14:28:26 -0300

    Ingestao de dados

--- zenon-fraud-detector/src/main/java/br/com/zenon/Main.java
+++ zenon-fraud-detector/src/main/java/br/com/zenon/Main.java
@@ -1,6 +1,7 @@
 package br.com.zenon;
 
 import java.math.BigDecimal;
+import java.util.List;
 
 public class Main {
 
@@ -16,6 +17,14 @@ public class Main {
 
         IO.println(t1);
         IO.println(t2);
+
+        IO.println("------------------------------------");
+
+        var transactionIngestor = new TransactionIngestor();
+        List<Transaction> transactions = transactionIngestor.read("data/PS_20174392719_1491204439457_log.csv");
+        IO.println(transactions.size());
+
+        transactions.stream().limit(10).forEach(IO::println);
 
     }
 }
