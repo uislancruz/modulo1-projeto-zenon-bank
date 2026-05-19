4003d330d52fde50a3388996e97e17dd6bed84ba
Uislan Cruz <uislansk8@hotmail.com>
2026-05-19 14:31:51 -0300

    ingestao de dados

--- zenon-fraud-detector/src/main/java/br/com/zenon/TransactionIngestor.java
+++ zenon-fraud-detector/src/main/java/br/com/zenon/TransactionIngestor.java
@@ -1,13 +1,10 @@
 package br.com.zenon;
 
-import java.io.File;
-import java.io.FileInputStream;
 import java.math.BigDecimal;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Scanner;
+
 
 public class TransactionIngestor {
 
