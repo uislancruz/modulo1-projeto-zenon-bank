823f79c022f3672df17f541455805232a397edd6
Uislan Cruz <uislansk8@hotmail.com>
2026-05-21 15:15:13 -0300

    Analise de fraude com stream

--- zenon-fraud-detector/src/main/java/br/com/zenon/Main.java
+++ zenon-fraud-detector/src/main/java/br/com/zenon/Main.java
@@ -2,6 +2,7 @@ package br.com.zenon;
 
 import java.math.BigDecimal;
 import java.util.List;
+import java.util.Map;
 
 public class Main {
 
@@ -33,6 +34,42 @@ public class Main {
 
         transactionsBadData.forEach(IO::println);
 
+        IO.println("------------------------------------");
+
+        var fraudAnalyzer = new FraudAnalyzer(transactions);
+
+        //Apenas transações onde isFraud == true, imprima o tamanho da lista
+        long fraudCount = fraudAnalyzer.countFrauds();
+        IO.println("Total de fraudes: " + fraudCount);
+
+        IO.println("------------------------------------");
+        //imprima as 3 fraudes de maior valor(amount)
+        List<BigDecimal> highestFraudsAmounts = fraudAnalyzer.findHighestValueFraudsAmounts(3);
+        IO.println("Top 3 fraudes com maior valor: ");
+        highestFraudsAmounts.forEach(amout -> IO.println(amout.toBigInteger() + " Doletas"));
+
+        IO.println("------------------------------------");
+
+        /*Obter apenas os nomes dos clientes de origem (nameOrig) dessas fraudes e depois gere uma
+        lista sem repetições (Set ou distinct) com os 5 maiores clientes suspeitos.*/
+        List<String> supiciousClients = fraudAnalyzer.findTopSuspiciousClients(5);
+        IO.println("Top 5 clientes suspeitos: ");
+        supiciousClients.forEach(IO::println);
+
+        IO.println("------------------------------------");
+
+        //Calcule o prejuízo total causado pelas fraudes (soma dos amount).
+
+        BigDecimal totalFraudLoss = fraudAnalyzer.calculateTotalFraudLoss();
+        IO.println("Prejuizo total: " + totalFraudLoss + " Doletas");
+
+        IO.println("------------------------------------");
+
+        //Conte quantas fraudes ocorreram por tipo de transação (CASH_OUT, TRANSFER, etc...).
+
+        Map<TransactionType, Long> fraudCountByType = fraudAnalyzer.countFraudsByType();
+        IO.println("Fraudes por tipo:");
+        fraudCountByType.forEach((type, count) -> IO.println("-" + type + ": " + count));
 
     }
 }
--- zenon-fraud-detector/src/main/java/br/com/zenon/TransactionIngestor.java
+++ zenon-fraud-detector/src/main/java/br/com/zenon/TransactionIngestor.java
@@ -9,13 +9,15 @@ import java.util.Optional;
 
 public class TransactionIngestor {
 
+    public static final int FRAUD_LIMIT = 50_000;
+
     public List<Transaction> read(String filename) {
         Path path = Path.of(filename);
         try {
             List<String> lines = Files.readAllLines(path);
             return lines.stream()
                     .skip(1)
-                    .limit(1000)
+                    .limit(FRAUD_LIMIT)
                     .map(this::parseTransaction)
                     .filter(Optional::isPresent)
                     .map(Optional::get)
